63ddfe19da45830f4c7e84e6c709964ca36faf96
Volha Kuralenia <Volha_Kuralenia@epam.com>
2021-04-07 14:21:46 +0300

    event added

--- StreamDecoration/Program.cs
+++ StreamDecoration/Program.cs
@@ -3,14 +3,24 @@ using System.IO;
 
 namespace StreamDecoration
 {
+    class Progress
+    {
+        public void PrintProgress(double number)
+        {
+            Console.WriteLine($"Progress is: {number} %");
+        }
+    }
+
     class Decoration : Stream
     {
-        private Stream streamDecorated;
+        private readonly Stream streamDecorated;
+        public delegate void ProgressCheck(double number);
+        public event ProgressCheck InProgress;
+
         public Decoration(Stream s)
         {
             streamDecorated = s;
         }
-
         public override bool CanRead => streamDecorated.CanRead;
 
         public override bool CanSeek => streamDecorated.CanSeek;
@@ -32,14 +42,30 @@ namespace StreamDecoration
 
         public override int Read(byte[] buffer, int offset, int count)
         {
-            EnterPassword();
-            if (streamDecorated.Length != streamDecorated.Position)
+            CheckPassword();
+            double percent = Math.Round((double)streamDecorated.Position / streamDecorated.Length * 100, 0);
+            switch (percent)
             {
-                Console.WriteLine($"\nProgress is: {Math.Round((double)streamDecorated.Position / streamDecorated.Length * 100, 2)}%");
-            }
-            else
-            {
-                Console.WriteLine($"\nProgress is: 100%");
+                case 10:
+                    InProgress(percent); break;
+                case 20:
+                    InProgress(percent); break;
+                case 30:
+                    InProgress(percent); break;
+                case 40:
+                    InProgress(percent); break;
+                case 50:
+                    InProgress(percent); break;
+                case 60:
+                    InProgress(percent); break;
+                case 70:
+                    InProgress(percent); break;
+                case 80:
+                    InProgress(percent); break;
+                case 90:
+                    InProgress(percent); break;
+                case 100:
+                    InProgress(percent); break;
             }
             return streamDecorated.Read(buffer, offset, count);
         }
@@ -59,11 +85,11 @@ namespace StreamDecoration
             streamDecorated.Write(buffer, offset, count);
         }
 
-        public void EnterPassword()
+        public static void CheckPassword()
         {
             Console.WriteLine("Enter password:");
             string line = Console.ReadLine();
-            if (line != "1111" || string.Empty == line)
+            if (line != "11" || string.Empty == line)
             {
                 throw new ArgumentException("\nWrong password! Acsess denied...");
             }
@@ -78,7 +104,10 @@ namespace StreamDecoration
                 string readPath = @"c:\Users\ollik\source\repos\DecorationPattern.txt";
                 using (Decoration decoration = new(new FileStream(readPath, FileMode.Open)))
                 {
-                    byte[] byteArray = new byte[500];
+                    Progress progress = new();
+                    decoration.InProgress += progress.PrintProgress;
+
+                    byte[] byteArray = new byte[200];
                     int byteResult;
                     do
                     {
